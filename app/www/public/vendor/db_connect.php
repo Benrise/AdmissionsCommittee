ae9cd06d47ca00abc74ce7830932f8a904eced9f
Benrise <benrise_x@mail.ru>
2023-03-23 06:32:01 +0300

    Minor changes, NoTransactions

--- app/www/public/vendor/db_connect.php
+++ app/www/public/vendor/db_connect.php
@@ -2,6 +2,6 @@
 $connect = mysqli_connect('database', 'root', 'tiger', 'AdmissionsCommittee');
 
 if (!$connect) {
-    die('Error connect to DataBase');
+    die('Error connect to DataBase, connection may in progress!');
 }
 
